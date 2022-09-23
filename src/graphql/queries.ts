a90283fdd9c1c2538b64330cceee86dc314b6498
LeSavantR <ruben_hernandez1993@hotmail.es>
2022-09-23 12:43:50 -0500

    xDD

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -3,6 +3,10 @@ import { gql } from "@apollo/client";
 export const ALL_PERSONS = gql`
 query {
   allPersons {
+    ... PersonDetail
+  }
+
+  fragment PersonDetail on Person {
     id
     name
     phone
